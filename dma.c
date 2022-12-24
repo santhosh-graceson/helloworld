03ed4bab81d5b83cc2063f81c58deab8fde7e2b8
udhayadhithan.pa <udhayadhithan.pa@aidenmedical.com>
2022-12-24 15:43:16 +0530

    new file:   dma.c new file:   dma.exe 	modified:   trail.c 	modified:   trail.exe 	new file:   trial2.c

--- dma.c
+++ dma.c
@@ -5,13 +5,20 @@ int main ()
     int n;
     printf("Enter the value of n:\t");
     scanf("%d",&n);
-    int *a= (int*)(malloc(n*(sizeof(int))));
+    int *a= (long*)(calloc(2*n,sizeof(long)));
     for (int i=0;i<n;i++)
     {
         a[i]=a[i+1];
     }
+    for (long i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    int *b=(int*)realloc(n*2,(sizeof(int)));
     for (int i=0;i<n;i++)
     {
-        printf("%d",a[i]);
+        a[i]=a[i+1];
+        printf("%d",b[n]);
     }
+    
 }
---
+++ trial2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void marks1(int )
+{
+    printf("The student A has scored")
+}
+int main()
+{
+
+}
